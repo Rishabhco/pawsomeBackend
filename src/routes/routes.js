59c75e718de7147866dcdd02648b2a5afc4b95de
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-04-06 02:06:35 +0530

    created routes and user model

---
+++ src/routes/routes.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const auth=require('../middleware/auth.js')
+const userController=require('../controller/userController.js')
+const petController=require('../controller/petController.js')
+const router=new express.Router()
+
+router.get('/',userController.home);
+router.post('/signup',userController.createUser);
+router.get('/readUser',auth,userController.readUser);
+router.post('/login',userController.login);
+router.post('/logout',auth,userController.logout);
+
+router.get('/petHome',petController.home);
+router.post('/adoptPet',auth,petController.adoptPet);
+router.get('/findAllPet',petController.findAllPet);
+router.get('/findSpecificPet',petController.findSpecificPet);
+router.get('/findSpecificPetCat',petController.findSpecificPetCat);
+
+module.exports=router
